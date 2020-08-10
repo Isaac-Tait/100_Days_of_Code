29b1b0f5f264df25e5c3db331c21115334e9882b
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-10 06:19:20 -0700

    phew, cranking on React this morning ☕️

---
+++ React/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Isaac Tait</h1>
+        <p>I enjoy tenkara fly fishing and rock climbing as well as trail running.</p>
+        <ul>
+          <li>Tohoku, Japan</li>
+          <li>Iceland</li>
+          <li>Torres del Paine</li>
+        </ul>
+      </div>
+    )
+  }
+
+  export default MyInfo
+
+  // Forked workspace with all my HardWork.js https://scrimba.com/c/c8MMrbtD
+  
