6c89fbd78653e6c8ae7dc3af53b90a35aa35644f
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-09 20:40:10 -0700

    Day 1 of 100 coding challenge

---
+++ Learning Rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/isaactait/Desktop/100_Days_of_Code/Learning Rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/isaactait/Desktop/100_Days_of_Code/Learning Rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ Learning Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/isaactait/Desktop/100_Days_of_Code/Learning\ Rust/hello_cargo/target/debug/hello_cargo: /Users/isaactait/Desktop/100_Days_of_Code/Learning\ Rust/hello_cargo/src/main.rs
