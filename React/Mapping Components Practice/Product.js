c09da900c097b06259abeae652f55a4d9a066f34
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-12 18:12:07 -0700

    added toFixed(2)

--- React/Mapping Components Practice/Product.js
+++ React/Mapping Components Practice/Product.js
@@ -5,7 +5,7 @@ function Product(props) {
         <div>
             <h3>You will need: </h3> <h1 className="h1">{props.name}</h1>
         
-            <h3>The price is:</h3> <h3 className="price">${props.price}</h3>
+            <h3>The price is:</h3> <h3 className="price">${props.price.toFixed(2)}</h3>
             
             <p>Product description: {props.description}</p>
             <hr />
