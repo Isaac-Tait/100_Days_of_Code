a5158f7b596fed568af6076e810a5238baba316a
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-12 22:28:45 -0700

    I am tired and am going to bed now 🥱

--- learning_rust/src/print.rs
+++ learning_rust/src/print.rs
@@ -1,4 +1,11 @@
 pub fn run() {
     // Print to console
-    println!("Hello from the print.rs file")
+    println!("Hello from the print.rs file");
+    // Basic formatting 
+    println!("Number: {}", 12);
+    println!("{} is {} year old", "Isaac", 37);
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}", 
+    "Isaac", "North County San Diego", "code"
+);
 }
