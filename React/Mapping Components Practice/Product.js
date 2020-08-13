4246ba2823e3137292b0dc7cb9a32807c088a58b
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-12 18:05:47 -0700

    mapping practice

---
+++ React/Mapping Components Practice/Product.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Product(props) {
+    return (
+        <div>
+            <h3>You will need: </h3> <h1 className="h1">{props.name}</h1>
+        
+            <h3>The price is:</h3> <h3 className="price">${props.price}</h3>
+            
+            <p>Product description: {props.description}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default Product
