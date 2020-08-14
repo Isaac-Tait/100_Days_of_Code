e1ef35043051575e311e42f2204fcf9652762812
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-14 07:16:08 -0700

    hmm

--- React/Todo_App/app.js
+++ React/Todo_App/app.js
@@ -19,13 +19,17 @@ class App extends React.Component {
         super()
         this.state = {
             todos: todosData
+            
         }
         this.handleChange = this.handleChange.bind(this)
     }
     
     handleChange(id) {
-        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+        this.setState(prevState => {
+            return (
+
+            )
+        })
     }
     
     render() {
