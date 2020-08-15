b6e9a132349c0ad9b0301b25ee2060d20600b74c
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-15 12:13:47 -0700

    Geez, this was a lot of work

--- React/Travel_Form/app.js
+++ React/Travel_Form/app.js
@@ -1,20 +1,5 @@
 import React, {Component} from "react"
 
-/**
- * Challenge: Wire up the partially-finished travel form so that it works!
- * Remember to use the concept of controlled forms
- * https://reactjs.org/docs/forms.html
- * 
- * All information should be populating the text below the form in real-time
- * as you're filling it out
- * 
- * This exercise is adapted from the V School curriculum on vanilla JS forms:
- * https://coursework.vschool.io/travel-form/
- * 
- * All of our challenges and learning resources are open for the public
- * to play around with and learn from at https://coursework.vschool.io
- */
-
 class App extends Component {
     constructor() {
         super()
@@ -23,8 +8,14 @@ class App extends Component {
             lastName: "",
             age: "",
             gender: "",
-            destination: "Tohoku, Japan",
-            restrictions: ""
+            destination: "",
+            restrictions: {
+                Vegan: false,
+                glutenFree: false,
+                lowFodMap: false,
+                Kosher: false
+
+            }
             
         }
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -42,8 +33,16 @@ class App extends Component {
         event.preventDefault();
     }
     
-    handleChange() {    
-        this.setState({value: event.target.value});  
+    handleChange(event) {    
+      const {name, value, type, checked} = event.target
+      type === "checkbox" ? 
+      this.setState({
+        [name]: checked
+      })
+      :
+      this.setState({
+        [name]: value
+      })
     }
     
     render() {
@@ -51,72 +50,90 @@ class App extends Component {
             <main>
                 <form>
                     <input 
-                        onChange={handleChange}
+                        onChange={this.handleChange}
                         type="text" 
                         name="firstName" 
+                        placeholder="First Name"
                         value={this.state.firstName}
-                    />
-                    <br />First Name<br />
-                    
+                    /><br />
                     <input 
-                        onChange={handleChange}
+                        onChange={this.handleChange}
                         type="text" 
                         name="lastName"
+                        placeholder="Last Name"
                         value={this.state.lastName}
-                    />
-                    <br />Last Name<br />
-                    
+                    /><br />
                     <input 
-                        onChange={handleChange}
+                        onChange={this.handleChange}
                         type="text" 
                         name="age"
+                        placeholder="Age"
                         value={this.state.age}
-                    />
-                    <br />Age< br/>
-                    
-                    <input 
-                        type="radio" 
-                    
-                    /><p>Male</p>
+                    /><br />
+                    <hr />
                     <br />
                     <input 
+                        onChange={this.handleChange}
                         type="radio" 
+                        name="gender"
+                        value="Male"
+                        checked={this.state.gender === "Male"}
+                    /> Male<br />
                     
-                    /><p>Female</p>
                     <input 
+                        onChange={this.handleChange}
                         type="radio" 
-                    
-                    /><p>Non-binary</p>
-                    <select value={this.state.destination} onChange={handleChange}>
+                        name="gender"
+                        value="Female"
+                        checked={this.state.gender === "Female"}
+                    /> Female<br />
+                    <input 
+                        onChange={this.handleChange}
+                        type="radio" 
+                        name="gender"
+                        value="Non-Binary"
+                        checked={this.state.gender === "Non-Binary"}
+                    />Non-binary<br />
+                    <hr />
+                    <select 
+                        value={this.state.destination} 
+                        name="destination"
+                        onChange={this.handleChange}
+                    >
+                        <option value="">-- Please Choose A Destination --</option>
                         <option value="Tohoku, Japan">Tohoku, Japan</option>
                         <option value="Jakarta, India">Jakarta, India</option>
                         <option value="Baffin Island">Baffin Island</option>
                         <option value="Inca, Peru">Inca, Peru</option>
                     </select>
                     <br />
-                    
+                    <hr />
                     <input 
                         type="checkbox" 
-                        value="Vegan" 
-                        onChange={handleChange}
-                     /><p>Vegan</p>
+                        name="Vegan" 
+                        onChange={this.handleChange}
+                        checked={this.state.Vegan}
+                     />Vegan <br />
                     <input 
                         type="checkbox" 
-                        value="Gluten-Free"
-                        onChange={handleChange}
-                    /><p>Gluten Free</p>
+                        name="glutenFree"
+                        onChange={this.handleChange}
+                        checked={this.state.glutenFree}
+                    />Gluten Free<br />
                     <input 
                         type="checkbox" 
-                        value="Low-F.O.D.M.A.P."
-                        onChange={handleChange}
-                    /><p>Low-F.O.D.M.A.P.</p>
+                        name="lowFodMap"
+                        onChange={this.handleChange}
+                        checked={this.state.lowFodMap}
+                    />Low-F.O.D.M.A.P.<br />
                     <input 
                         type="checkbox"
-                         value="Kosher"
-                        onChange={handleChange}
-                    /><p>Kosher</p>
+                         name="Kosher"
+                        onChange={this.handleChange}
+                        checked={this.state.Kosher}
+                    />Kosher<br />
                     <br />
-                    
+                    <hr />
                     <button onClick={this.handleSubmit}>Submit</button>
                 </form>
                 
@@ -126,10 +143,11 @@ class App extends Component {
                 <p>Your age: {this.state.age}</p>
                 <p>Your gender: {this.state.gender}</p>
                 <p>Your destination: {this.state.destination}</p>
-                <p>
-                    Your dietary restrictions: 
-                    {this.state.restrictions}
-                </p>
+                <h3>Your dietary restrictions: </h3>
+                    <p>Vegan: {this.state.Vegan ? "Yes" : "No"}</p>
+                    <p>Gluten Free: {this.state.glutenFree ? "Yes" : "No"}</p>
+                    <p>Low-FODMAP: {this.state.lowFodMap ? "Yes" : "No"}</p>
+                    <p>Kosher: {this.state.Kosher ? "Yes" : "No"}</p>
             </main>
         )
     }
