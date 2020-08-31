eb7a497555528645f5ef2a911711465801045f2b
Isaac Tait <isaac@mountaintopcoding.com>
2020-08-31 16:49:22 -0700

    JavaScript challenges

---
+++ Brushing Up on JavaScript/chapterTwo.js
@@ -0,0 +1,15 @@
+// FizzBuzz
+for (number = 1; number <= 100; number++) {
+    if (number % 3 == 0 && number % 5 == 0) console.log("FizzBuzz")
+        else if (number % 5 == 0) console.log("Buzz")
+        else if (number % 3 == 0) console.log("Fizz") 
+        else console.log(number)
+}
+
+// Looping a triangle
+let hash = "#";
+
+for (counter = 0; counter < 8; counter++) {   
+};
+
+console.log(counter);
